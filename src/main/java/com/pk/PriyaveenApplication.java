613a1290a91363fddab8072169956be2d5b1e528
Praveen <Praveen@DESKTOP-6NO0972>
2020-04-03 07:55:43 +0530

    initial commit

---
+++ src/main/java/com/pk/PriyaveenApplication.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriyaveenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriyaveenApplication.class, args);
+	}
+	
+}
