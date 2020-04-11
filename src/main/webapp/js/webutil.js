3246484de9fbbdeda11c55ca1dbad5fdbc87d0f9
Praveen <Praveen@DESKTOP-6NO0972>
2020-04-11 23:00:09 +0530

    Q n A

---
+++ src/main/webapp/js/webutil.js
@@ -0,0 +1,19 @@
+
+
+
+//check GSTIN Number
+function checksum(g){
+    let regTest = /\d{2}[A-Z]{5}\d{4}[A-Z]{1}[A-Z\d]{1}[Z]{1}[A-Z\d]{1}/.test(g)
+     if(regTest){
+        let a=65,b=55,c=36;
+        return Array['from'](g).reduce((i,j,k,g)=>{ 
+           p=(p=(j.charCodeAt(0)<a?parseInt(j):j.charCodeAt(0)-b)*(k%2+1))>c?1+(p-c):p;
+           return k<14?i+p:j==((c=(c-(i%c)))<10?c:String.fromCharCode(c+b));
+        },0); 
+    }
+    return regTest
+}
+
+//console.log(checksum('27AAPFU0939F1ZV'))//true
+//console.log(checksum('27AASCS2460H1Z0'))//false
+//console.log(checksum('29AAGCB7383J1Z4'))//true
